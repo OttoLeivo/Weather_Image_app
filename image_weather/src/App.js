a46239bb4cfdb6c6a566f4cc91e728d5fa0a0d47
Otto Leivo <0tt0.l31v0@gmail.com>
2020-03-06 17:27:06 +0200

    api key added

--- image_weather/src/App.js
+++ image_weather/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Weather from "./components/weather.component"
 
 function App() {
+  console.log(process.env)
   return (
+    
     <div className="App">
       <header className="App-header">
         <Weather />
--- image_weather/src/components/weather.component.jsx
+++ image_weather/src/components/weather.component.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+require('dotenv').config();
 
+const api_key = process.env.REACT_APP_API_KEY;
 class Weather extends Component {
+
     state = {  }
     render() { 
+        console.log(`http://api.openweathermap.org/data/2.5/weather?q=helsinki&appid=${api_key}`)
         return ( <div>
             <h1>QUITE RAINY</h1>
+            
         </div> );
     }
 }
