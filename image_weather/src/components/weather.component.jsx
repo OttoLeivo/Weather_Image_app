17d5bda1879a68835d10b5ff631848800a2fa80c
Otto Leivo <0tt0.l31v0@gmail.com>
2020-03-06 14:11:51 +0200

    quite rainy

---
+++ image_weather/src/components/weather.component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Weather extends Component {
+    state = {  }
+    render() { 
+        return ( <div>
+            <h1>QUITE RAINY</h1>
+        </div> );
+    }
+}
+ 
+export default Weather;
