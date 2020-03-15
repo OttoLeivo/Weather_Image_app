ee8a072b138db2f2f20d3aeaaa2490008e6a6ab9
OttoLeivo <0tt0.l31v0@gmail.com>
2020-03-15 11:44:44 +0200

    temperature works

--- image_weather/src/App.js
+++ image_weather/src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import './App.css';
-import Weather from "./components/weather.component"
+import React from "react";
+import "./App.css";
+import Weather from "./components/weather.component";
 
 function App() {
-  console.log(process.env)
+  //console.log(process.env)
   return (
-    
     <div className="App">
       <header className="App-header">
         <Weather />
--- image_weather/src/components/weather.component.jsx
+++ image_weather/src/components/weather.component.jsx
@@ -1,17 +1,42 @@
-import React, { Component } from 'react';
-require('dotenv').config();
+import React, { Component } from "react";
+require("dotenv").config();
 
 const api_key = process.env.REACT_APP_API_KEY;
+let temperature = "";
 class Weather extends Component {
+  constructor() {
+    super();
+    this.state = {
+      city: "sunny af"
+    };
+  }
 
-    state = {  }
-    render() { 
-        console.log(`http://api.openweathermap.org/data/2.5/weather?q=helsinki&appid=${api_key}`)
-        return ( <div>
-            <h1>QUITE RAINY</h1>
-            
-        </div> );
-    }
+  //after component mounted get api and re render
+  componentDidMount() {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=helsinki&appid=${api_key}`
+    )
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        console.log(data.name);
+        temperature = KelvinToCelsius(data.main.temp);
+        this.setState({ city: data.name });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.city}</h1>
+        <h1>{temperature}&deg;</h1>
+      </div>
+    );
+  }
 }
- 
+
+function KelvinToCelsius(data1) {
+  return Math.floor(data1 - 274.15);
+}
+
 export default Weather;
